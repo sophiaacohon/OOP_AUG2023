7e26b18be10ee9a45c51d78cc995d53b46f6d688
Soph <111960629+sophiaacohon@users.noreply.github.com>
2023-09-28 23:20:54 +0800

    updated Name class

--- EmployeeProject/EmployeeProject/src/employee/version4/Name.java
+++ EmployeeProject/EmployeeProject/src/employee/version4/Name.java
@@ -9,5 +9,35 @@ package employee.version4;
  * @author Sophia
  */
 public class Name {
-    
+    String title;
+    String firstName;
+    String middleName;
+    String lastName;
+    String suffix;
+
+    public Name(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public Name(String title, String firstName, String middleName, String lastName) {
+        this.title = title;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public Name(String title, String firstName, String middleName, String lastName, String suffix) {
+        this.title = title;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.suffix = suffix;
+    }
+
+    @Override
+    public String toString() {
+        return lastName + ", " + firstName + " " + middleName.charAt(0) + ". " + suffix;
+    }
 }
